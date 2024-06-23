27552528dd394030e0118d81d08cd93e46b2d173
bk-veselinov <153025470+bk-veselinov@users.noreply.github.com>
2024-06-23 21:10:44 +0300

    Added helper classes
    classes added:
    MyString
    Vector
    polymorphic_ptr
    polymorphic_container

---
+++ polymorphic_container.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "polymorphic_ptr.hpp"
+#include "Vector.hpp"
+
+template<typename T>
+class polymorphic_container {
+private:
+	Vector<polymorphic_ptr<T>> ptrs;
+
+public:
+	void add(T* ptr);
+	size_t getPtrsCount() const;
+	T& operator[](size_t index) const;
+	/*void execute(size_t index, void(*func)(T*));
+	void execute(size_t index, void(*func)(const T*)) const;*/
+};
+
+template<typename T>
+void polymorphic_container<T>::add(T* ptr) {
+	ptrs.push_back(ptr);
+}
+template<typename T>
+size_t polymorphic_container<T>::getPtrsCount() const {
+	return ptrs.getSize();
+}
+
+template<typename T>
+T& polymorphic_container<T>::operator[](size_t index) const{
+	return *ptrs[index];
+}
+
+//template<typename T>
+//void polymorphic_container<T>::execute(size_t index, void(*func)(T*)) {
+//	func(ptrs[index].get());
+//}
+//
+//
+//template<typename T>
+//void polymorphic_container<T>::execute(size_t index, void(*func)(const T*)) const {
+//	func(ptrs[index].get());
+//}
